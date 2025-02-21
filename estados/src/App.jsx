3b29094c7bb37a68fab4422e90b47544efe5cdc2
erikkavulack <kavulacke@gmail.com>
2025-02-20 21:51:18 -0300

    20/02

---
+++ estados/src/App.jsx
@@ -0,0 +1,33 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [usuario, setUsuario] = useState("Juca")
+
+  let nome = "Ulib"
+
+  function lerNome(){
+    nome = prompt("Digite o nome (vou ignorar):")
+    console.log(nome);
+    
+  }
+  function lerUsuario(){
+    let resposta = prompt("Digite o novo usúario:")
+    setUsuario(resposta)
+  }
+
+  return (
+    <>
+      <h1>ESTADOS</h1>
+      Nome: {nome}
+      <div>
+        Usúario: {usuario}
+      </div>
+      <button onClick={lerNome}>Trocar nome</button>
+      <button onClick={lerUsuario}>Trocar usúario</button>
+    </>
+  )
+}
+
+export default App
