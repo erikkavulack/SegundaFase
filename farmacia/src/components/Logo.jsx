a7f2da283f15cf99babd31e991464e72a097e0fd
erikkavulack <kavulacke@gmail.com>
2025-04-30 21:59:27 -0300

    Desenvolvimento projeto farmácia

---
+++ farmacia/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import './Logo.css'
+
+function Logo() {
+  return (
+    <img src="/img/farmarcia-logo-pq.PNG.png" alt="logo da farmácia" className='img-Logo'/>
+  )
+}
+
+export default Logo
---
+++ farmacia/src/components/Sorteio.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './Sorteio.css'
+import { useEffect } from 'react'
+
+function Sorteio() {
+    const[inputNome, setInputNome] = useState('')
+    const[nomes, setNomes] = useState ([])
+
+    // useEffect{() => console.log(nomes), [nomes]}
+
+function cadastrarNome(){
+    setNomes([inputNome, ...nomes])
+    // console.log(nomes);
+    
+}
+
+  return (
+    <div className='container-sorteio'>
+        <h2>SORTEIO !!</h2>
+        <p>Increva-se aqui para o sorteio de uma linda camiseta</p>
+
+        <label htmlFor="Nome"></label>
+        <input type="text" value={inputNome} onChange={(event) => setInputNome(event.target.value)}/>
+        <button onClick={cadastrarNome}>Cadastrar</button>
+
+    </div>
+  )
+}
+
+export default Sorteio
