b58669a0bbe4b22bd1d4c20c851ae1774968640e
erikkavulack <kavulacke@gmail.com>
2025-05-06 21:54:18 -0300

    Começando o projeto iMesada

---
+++ iMesada/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='container-body'>
+        <h2>iMesada</h2>
+
+    </div>
+  )
+}
+
+export default Body
